483a21d6256f9e2fb95562f3c604ba0f8acd002b
João Silva <104035680+jpeodr@users.noreply.github.com>
2024-06-29 06:45:18 -0400

    Add files via upload

---
+++ condicional_par_ou_impar/Exercicios/Exercicios/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro: ");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Par");
+
+            }
+            else
+            {
+                Console.WriteLine("Impar");
+            }
+        }
+    }
+}
