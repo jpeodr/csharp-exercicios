0bf7be26b41747eda087de3c62c536d3d4126fae
João Silva <104035680+jpeodr@users.noreply.github.com>
2024-06-29 08:05:06 -0400

    Add files via upload

---
+++ condicional_horario/Exercicios/Exercicios/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (hora >= 12 && hora < 18)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            else if (hora >= 18 && hora < 24)
+            {
+                Console.WriteLine("Boa noite!");
+            }
+            else
+            {
+                Console.WriteLine("Hora inválida!");
+            }
+        }
+    }
+}
