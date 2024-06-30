c2db15d922752f5789da7adec5d699eef5f0ac61
João Silva <104035680+jpeodr@users.noreply.github.com>
2024-06-29 22:41:28 -0400

    Add files via upload

---
+++ condicional_positivo_negativo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero == 0)
+            {
+                Console.WriteLine("Não negativo");
+            }
+            else if (numero > 0)
+            {
+                Console.WriteLine("Não negativo");
+            }
+            else
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+        }
+    }
+}
